33c582137481f2be50217fa4ff85420f2f9d8a80
DJ & DK <87873024+DJ-DK@users.noreply.github.com>
2022-04-08 16:28:20 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCXdDD0fZgPSWyeA1OBcOIUxZZ3CuU7huM",
+    authDomain: "s53r5t.firebaseapp.com",
+    databaseURL: "https://s53r5t-default-rtdb.firebaseio.com",
+    projectId: "s53r5t",
+    storageBucket: "s53r5t.appspot.com",
+    messagingSenderId: "808993206526",
+    appId: "1:808993206526:web:92931aeda8dccd5733b5af"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
